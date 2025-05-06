9acc2a0a6c66deaba15836db782bf16ff79a3959
qswndyyy <wendyquispesoria@gmail.com>
2025-05-05 22:58:33 -0300

    Se creo la funcion de mostrar la fecha actual.

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def saludar():
     nombre = input("\n¿Cuál es tu nombre?: ")
     print(f"\n¡Hola, {nombre}! Esta es la función de saludo.\n")
 
+def mostrar_fecha():
+    fecha_actual = datetime.datetime.now().strftime("%d-%m-%Y")
+    print(f"\nLa fecha actual es: {fecha_actual}\n")
+
 def mostrar_menu():
   print("\nMenu de opciones: ")
   print("1. Saludar")
@@ -20,7 +24,7 @@ def main():
       case "1":
         saludar()
       case "2":
-        print("Funcion de Mostrar fecha")
+        mostrar_fecha()
       case "3":
         os.system("cls")
         print("\n\nSaliendo...\n\n")
